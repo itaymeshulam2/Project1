9dfedcf116f8610442254555e2d753aab4475bd5
itay <itaymeshulam2@gmail.com>
2020-02-09 02:11:05 +0200

    first commit

--- src/test/java/com/example/projects/AutomationTest.java
+++ src/test/java/com/example/projects/AutomationTest.java
@@ -5,29 +5,31 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.Keys;
 
-import static org.hamcrest.CoreMatchers.is;
-import static org.junit.Assert.assertThat;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+
 
 public class AutomationTest {
 
-    @Test
-    public void test() {
-        System.setProperty("webdriver.chrome.driver", "C:\\Users\\Itay\\Desktop\\selenimDrivers\\chromedriver.exe");
-        WebDriver driver = new ChromeDriver();
-        driver.get("http://localhost:8181/itay_meshulam/");
-        driver.manage().window().setSize(new Dimension(708, 728));
-        driver.findElement(By.name("author")).click();
-        driver.findElement(By.cssSelector("input:nth-child(4)")).click();
-        assertThat(driver.findElement(By.cssSelector("li")).getText(), is("Tan Ah Teck"));
-        driver.findElement(By.linkText("BACK")).click();
-        driver.findElement(By.cssSelector("input:nth-child(3)")).click();
-        driver.findElement(By.cssSelector("input:nth-child(4)")).click();
-        assertThat(driver.findElement(By.cssSelector("li")).getText(), is("Kumar"));
-        driver.findElement(By.linkText("BACK")).click();
-        driver.findElement(By.cssSelector("input:nth-child(2)")).click();
-        driver.findElement(By.cssSelector("input:nth-child(4)")).click();
-        assertThat(driver.findElement(By.cssSelector("li")).getText(), is("Mohd Ali"));
-    }
+
+        @Test
+        public void  Automation() {
+            System.setProperty("webdriver.chrome.driver", "C:\\Users\\Itay\\Desktop\\selenimDrivers\\chromedriver.exe");
+            WebDriver driver = new ChromeDriver();
+            driver.get("http://localhost:8081/admin/checks.xhtml?page=18");
+
+            driver.findElement(By.name("username")).sendKeys("admin");
+            driver.findElement(By.name("username")).sendKeys(Keys.ENTER);
+            driver.findElement(By.name("password")).sendKeys("admin");
+            driver.findElement(By.name("password")).sendKeys(Keys.ENTER);
+            driver.findElement(By.cssSelector(".fa-play")).click();
+
+
 
 }
+
+        }
+
+
